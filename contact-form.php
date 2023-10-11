d6fa0394943e62e6712bdb1a881854ceb8a1561e
sametakalinkod <107934311+sametakalinkod@users.noreply.github.com>
2023-10-11 09:05:33 +0300

    .

--- contact-form.php
+++ contact-form.php
@@ -1,34 +1,37 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $name = $_POST["name"];
+//     $email = $_POST["email"];
+//     $subject = $_POST["subject"];
+//     $message = $_POST["message"];
 
-    $_SERVER["REQUEST_URL"] = "http://localhost:5010/services/identity/api/account/UpdateDatabase";
 
-    // Set the recipient's email address
-    $to = "samet.akalin@kod.com.tr"; // Replace with the actual email address.
+//     // Set the recipient's email address
+//     $to = "samet.akalin@kod.com.tr"; // Replace with the actual email address.
 
-    // Email headers
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+//     // Email headers
+//     $headers = "From: $email\r\n";
+//     $headers .= "Reply-To: $email\r\n";
+//     $headers .= "MIME-Version: 1.0\r\n";
+//     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-    // Compose the email message
-    $email_message = "Name: $name<br>";
-    $email_message .= "Email: $email<br>";
-    $email_message .= "Subject: $subject<br><br>";
-    $email_message .= "Message:<br>$message";
+//     // Compose the email message
+//     $email_message = "Name: $name<br>";
+//     $email_message .= "Email: $email<br>";
+//     $email_message .= "Subject: $subject<br><br>";
+//     $email_message .= "Message:<br>$message";
 
-    // Send the email
-    if (mail($to, $subject, $email_message, $headers)) {
-        // Email sent successfully
-        echo "success"; // You can customize this response as needed.
-    } else {
-        // Email sending failed
-        echo "error"; // You can customize this response as needed.
-    }
+//     // Send the email
+//     if (mail($to, $subject, $email_message, $headers)) {
+//         // Email sent successfully
+//         echo "success"; // You can customize this response as needed.
+//     } else {
+//         // Email sending failed
+//         echo "error"; // You can customize this response as needed.
+//     }
+// }
+if($_POST["message"]) {
+    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
 }
+
 ?>
